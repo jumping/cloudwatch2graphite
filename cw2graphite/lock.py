cfc0ab9920634c0dded74de0e007f58f9df047ec
J_Qu <410863514@qq.com>
2015-12-18 13:06:55 +0800

    added lock.py

---
+++ cw2graphite/lock.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+import fcntl  
+  
+class ProcessLock(object):  
+  
+    __lockfd = None  
+ 
+    @staticmethod  
+    def lock():  
+            ProcessLock.__lockfd = open(__file__, 'a+')  
+            fcntl.flock(ProcessLock.__lockfd, fcntl.LOCK_EX)  
+ 
+    @staticmethod  
+    def unlock():  
+            fcntl.flock(ProcessLock.__lockfd, fcntl.LOCK_UN) 
